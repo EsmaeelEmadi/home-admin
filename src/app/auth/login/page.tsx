a15ca016b97bb2e39b92dccedd20e50940aa9d0d
emad <emadi68moh@gmail.com>
2024-05-31 16:32:10 +0330

    feat: init LoginPage

---
+++ src/app/auth/login/page.tsx
@@ -0,0 +1,11 @@
+// ── types
+import type { FC } from "react";
+
+//          ╭─────────────────────────────────────────────────────────╮
+//          │                        component                        │
+//          ╰─────────────────────────────────────────────────────────╯
+const LoginPage: FC = () => {
+  return <h1>login page</h1>
+};
+
+export default LoginPage;
