d15c20907a162a5fb00140e16c9ad2a4a0e8256e
emad <emadi68moh@gmail.com>
2024-05-31 15:32:23 +0330

    feat: init rootPage

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+// ── types
+import type { FC } from "react";
+
+//          ╭─────────────────────────────────────────────────────────╮
+//          │                        component                        │
+//          ╰─────────────────────────────────────────────────────────╯
+const RootPage: FC = () => {
+  return <h1>Hello World</h1>;
+};
+
+export default RootPage;
