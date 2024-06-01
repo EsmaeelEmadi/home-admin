96dfdbaee0ac3764fa608c96fddb58d5703766ac
emad <emadi68moh@gmail.com>
2024-06-01 23:26:03 +0330

    feat: implementing basic login ui

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,11 +1,90 @@
-// ── types
+"use client";
+
+// types
 import type { FC } from "react";
+// components
+import { Button, Checkbox, Form, Input, Typography } from "antd";
+import Link from "next/link";
+// icons
+import { LockOutlined, UserOutlined } from "@ant-design/icons";
+import { LogoTypeSvg } from "@/svgs";
 
 //          ╭─────────────────────────────────────────────────────────╮
 //          │                        component                        │
 //          ╰─────────────────────────────────────────────────────────╯
+
+const { Title, Text } = Typography;
+
 const LoginPage: FC = () => {
-  return <h1>login page</h1>
+  const onFinish = (values: any) => {
+    console.log("Received values of form: ", values);
+  };
+
+  return (
+    <div>
+      <LogoTypeSvg className="ml-8 w-48 fill-blue-600 h-fit mb-8" />
+      <div className="p-14 bg-white shadow-2xl rounded-xl bg-[url('/images/jpg/bg.jpg')]">
+        <div className="mb-6">
+          <Title level={3} className="mb-1">
+            Login
+          </Title>
+          <Text>Here is some subtext</Text>
+        </div>
+
+        <Form
+          name="normal_login"
+          className="login-form"
+          initialValues={{ remember: true }}
+          onFinish={onFinish}
+          style={{ width: 350 }}
+        >
+          <Form.Item
+            name="username"
+            rules={[{ required: true, message: "Please input your Username!" }]}
+          >
+            <Input
+              prefix={<UserOutlined className="site-form-item-icon" />}
+              placeholder="Username"
+            />
+          </Form.Item>
+          <Form.Item
+            name="password"
+            rules={[{ required: true, message: "Please input your Password!" }]}
+          >
+            <Input
+              prefix={<LockOutlined className="site-form-item-icon" />}
+              type="password"
+              placeholder="Password"
+            />
+          </Form.Item>
+          <Form.Item>
+            <div className="flex place-content-between">
+              <Form.Item name="remember" valuePropName="checked" noStyle>
+                <Checkbox>Remember me</Checkbox>
+              </Form.Item>
+
+              <Link href="/auth/forget-password">Forgot password</Link>
+            </div>
+          </Form.Item>
+
+          <Form.Item>
+            <div className="flex flex-col gap-2">
+              <Button
+                type="primary"
+                htmlType="submit"
+                className="login-form-button"
+              >
+                Log in
+              </Button>
+              <div>
+                Or <Link href="/auth/register">register now!</Link>
+              </div>
+            </div>
+          </Form.Item>
+        </Form>
+      </div>
+    </div>
+  );
 };
 
 export default LoginPage;
