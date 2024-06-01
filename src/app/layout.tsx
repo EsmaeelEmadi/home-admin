192e0b50f73465d1034ffd215d9f73808bf0799a
emad <emadi68moh@gmail.com>
2024-06-01 20:48:50 +0330

    fea: added ant design config providers

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,27 @@
 import { AntdRegistry } from "@ant-design/nextjs-registry";
 
 // ── components
+import { App, ConfigProvider, Flex } from "@/ant";
 
 // ── types
 import type { FC, PropsWithChildren } from "react";
+
+// ── styles
+import ANT_DEFAULT_THEME from "@/themes/ant/default";
+import "./index.scss";
 
 //          ╭─────────────────────────────────────────────────────────╮
 //          │                        component                        │
 //          ╰─────────────────────────────────────────────────────────╯
 const RootLayout: FC<PropsWithChildren> = ({ children }) => {
   return (
-    <html lang="en">
-      <body>
-        <div>
-          <AntdRegistry>{children}</AntdRegistry>
-        </div>
+    <html lang="en" className="h-full">
+      <body className="m-0 h-full">
+        <AntdRegistry>
+          <ConfigProvider theme={ANT_DEFAULT_THEME}>
+            <App className="h-full">{children}</App>
+          </ConfigProvider>
+        </AntdRegistry>
       </body>
     </html>
   );
