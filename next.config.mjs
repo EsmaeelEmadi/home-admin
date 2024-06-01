cfd584ec589684c6816f3ffcee264d31a7fc9061
emad <emadi68moh@gmail.com>
2024-06-01 20:42:24 +0330

    feat: added the capability to load svgs

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,26 @@
+import path from "path";
+import url from 'url';
+
+const filename = url.fileURLToPath(import.meta.url);
+const dirname = path.dirname(filename);
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	reactStrictMode: true,
-	swcMinify: true,
-	output: 'export',
-	images: { unoptimized: true }
+  // images: { unoptimized: true }
+  reactStrictMode: true,
+  swcMinify: true,
+  output: "export",
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"],
+    });
+
+    return config;
+  },
+  sassOptions: {
+    includePaths: [path.join(dirname, "styles")],
+  },
 };
 
 export default nextConfig;
