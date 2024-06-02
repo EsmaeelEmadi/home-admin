a9174fd067d6ca282255d49700c7ea0a2633d7dc
emad <emadi68moh@gmail.com>
2024-06-02 17:43:25 +0330

    feat: init register ui

---
+++ src/app/auth/register/page.tsx
@@ -0,0 +1,96 @@
+"use client";
+
+// types
+import type { FC } from "react";
+// components
+import { Button, Form, Input, Typography } from "antd";
+import Link from "next/link";
+// icons
+import { LockOutlined, UserOutlined, MailOutlined } from "@ant-design/icons";
+import { LogoTypeSvg } from "@/svgs";
+
+//          ╭─────────────────────────────────────────────────────────╮
+//          │                        component                        │
+//          ╰─────────────────────────────────────────────────────────╯
+
+const { Title, Text } = Typography;
+
+const RegisterPage: FC = () => {
+  const onFinish = (values: any) => {
+    console.log("Received values of form: ", values);
+  };
+
+  return (
+    <div>
+      <LogoTypeSvg className="ml-8 w-48 fill-blue-600 h-fit mb-4" />
+      <div className="p-14 bg-white shadow-2xl rounded-xl bg-[url('/images/jpg/bg.jpg')]">
+        <div className="mb-6">
+          <Title level={3} className="mb-1">
+            Register
+          </Title>
+          <Text>Here is some subtext</Text>
+        </div>
+
+        <Form
+          name="normal_register"
+          className="register-form"
+          onFinish={onFinish}
+          style={{ width: 350 }}
+        >
+          <Form.Item
+            name="email"
+            rules={[
+              {
+                required: true,
+                type: "email",
+                message: "Please input your Email!",
+              },
+            ]}
+          >
+            <Input
+              prefix={<MailOutlined className="site-form-item-icon" />}
+              placeholder="Email"
+            />
+          </Form.Item>
+          <Form.Item
+            name="username"
+            rules={[{ required: true, message: "Please input your Username!" }]}
+          >
+            <Input
+              prefix={<UserOutlined className="site-form-item-icon" />}
+              placeholder="Username"
+            />
+          </Form.Item>
+          <Form.Item
+            name="password"
+            rules={[{ required: true, message: "Please input your Password!" }]}
+          >
+            <Input
+              prefix={<LockOutlined className="site-form-item-icon" />}
+              type="password"
+              placeholder="Password"
+            />
+          </Form.Item>
+
+          <Form.Item>
+            <div className="flex flex-col gap-2">
+              <Button
+                type="primary"
+                htmlType="submit"
+                className="register-form-button"
+              >
+                Register
+              </Button>
+              <div>
+                Or <Link href="/auth/register">Login</Link> If you have an
+                account
+              </div>
+            </div>
+          </Form.Item>
+        </Form>
+      </div>
+    </div>
+  );
+};
+
+export default RegisterPage;
