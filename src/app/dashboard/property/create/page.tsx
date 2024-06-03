5bcf64678e642f74fe8171e9ceaacb92b5de7079
emad <emadi68moh@gmail.com>
2024-06-03 05:49:42 +0330

    feat: init create property page

---
+++ src/app/dashboard/property/create/page.tsx
@@ -0,0 +1,12 @@
+// types
+import type { FC } from "react";
+
+const PropertyPage: FC = () => {
+  return (
+    <div className="h-full bg-white dark:bg-dark p-8 rounded-3xl">
+      <h1>Create property</h1>
+    </div>
+  );
+};
+
+export default PropertyPage;
