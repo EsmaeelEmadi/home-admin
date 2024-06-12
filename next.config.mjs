4794fb31843e7f51e256589debb8723a549c5f3b
emad <emadi68moh@gmail.com>
2024-06-12 21:21:25 +0330

    fix: removing output option

--- next.config.mjs
+++ next.config.mjs
@@ -1,26 +1,25 @@
-import path from "path";
-import url from 'url';
+ import path from "path";
+ import url from 'url';
 
-const filename = url.fileURLToPath(import.meta.url);
-const dirname = path.dirname(filename);
+ const filename = url.fileURLToPath(import.meta.url);
+ const dirname = path.dirname(filename);
+ 
+ /** @type {import('next').NextConfig} */
+ const nextConfig = {
+   reactStrictMode: true,
+   swcMinify: true,
+   webpack(config) {
+     config.module.rules.push({
+       test: /\.svg$/,
+       use: ["@svgr/webpack"],
+     });
+ 
+     return config;
+   },
+   sassOptions: {
+     includePaths: [path.join(dirname, "styles")],
+   },
+ };
+ 
+ export default nextConfig;
 
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  // images: { unoptimized: true }
-  reactStrictMode: true,
-  swcMinify: true,
-  output: "export",
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/,
-      use: ["@svgr/webpack"],
-    });
-
-    return config;
-  },
-  sassOptions: {
-    includePaths: [path.join(dirname, "styles")],
-  },
-};
-
-export default nextConfig;
