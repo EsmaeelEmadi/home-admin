3825322b3b19f18ea080429ce1a2ddf03c5268d4
emad <emadi68moh@gmail.com>
2024-06-16 17:39:29 +0330

    refactor: removing unnecessary code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,17 @@
 "use client";
 
-// components
-import { Layout } from "@/ant";
-
 // types
 import type { FC } from "react";
 
-const { Content } = Layout;
-
 //          ╭─────────────────────────────────────────────────────────╮
-//          │                        component                             │
+//          │                        component                        │
 //          ╰─────────────────────────────────────────────────────────╯
 
 /**
  * NOTE: this page will be removed
  */
 const RootPage: FC = () => {
-  return (
-    <Content>
-      <h1>holy moly</h1>
-    </Content>
-  );
+  return null;
 };
 
 export default RootPage;
