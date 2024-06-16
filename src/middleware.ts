db323a42da5be440c53ca971e9516e5635507730
emad <emadi68moh@gmail.com>
2024-06-16 17:38:31 +0330

    reat: redirect if pathName is '/' to '/dashboard'

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,21 +1,12 @@
 import { type NextRequest, NextResponse } from "next/server";
 
 export default function middleware(request: NextRequest) {
-  const currentUser = null;
-
-  if (currentUser && request.nextUrl.pathname === "/") {
+  if (request.nextUrl.pathname === "/") {
     return NextResponse.redirect(new URL("/dashboard", request.url));
   }
 
-  if (currentUser && request.nextUrl.pathname !== "/") {
-    return NextResponse.redirect(new URL("/auth/login", request.url));
-  }
-
-  if (currentUser && !request.nextUrl.pathname.startsWith("/dashboard")) {
-    return NextResponse.redirect(new URL("/dashboard", request.url));
-  }
-
-  return NextResponse.redirect(new URL("/auth/login", request.url));
+  // default case
+  return NextResponse.next();
 }
 
 export const config = {
