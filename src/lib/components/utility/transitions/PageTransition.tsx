213357b39b13f0b3cd230312e12cae3dd1410747
emad <emadi68moh@gmail.com>
2024-06-17 23:12:56 +0330

    feat: added property className

--- src/lib/components/utility/transitions/PageTransition.tsx
+++ src/lib/components/utility/transitions/PageTransition.tsx
@@ -3,10 +3,16 @@
 import { motion } from "framer-motion";
 import type { ReactNode } from "react";
 
-export default function ({ children }: { children: ReactNode }) {
+export default function ({
+  children,
+  className,
+}: {
+  children: ReactNode;
+  className?: string;
+}) {
   return (
     <motion.div
-      className="h-full"
+      className={className}
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
       transition={{ ease: "easeInOut", duration: 0.5 }}
