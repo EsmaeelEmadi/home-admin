96d13c08f128cf38ef7c6fc914eb0ca23387a6c9
emad <emadi68moh@gmail.com>
2024-06-26 14:56:13 +0330

    fix: miss use document

--- src/lib/components/utility/useDimension/useDimension.ts
+++ src/lib/components/utility/useDimension/useDimension.ts
@@ -22,38 +22,43 @@ export const useDimension = () => {
   });
 
   useEffect(() => {
+    if (typeof window === "undefined") return;
+
     const handleResize = (e: UIEvent | Window) => {
-      const _window: Window = e instanceof Window ? e : (e.target as Window);
-      const w = _window.innerWidth;
-      const h = _window.innerHeight;
+      const _window: Window | undefined =
+        e instanceof Window ? e : (e.target as Window);
+      if (_window) {
+        const w = _window.innerWidth;
+        const h = _window.innerHeight;
 
-      let media: IMedia;
-      if (/iPhone|iPad|iPod|Android/i.test(navigator.userAgent)) {
-        media = "sm";
-      } else if (w <= 640) {
-        media = "sm";
-      } else if (w <= 768) {
-        media = "md";
-      } else if (w <= 1024) {
-        media = "lg";
-      } else if (w <= 1280) {
-        media = "xl";
-      } else {
-        media = "2xl";
+        let media: IMedia;
+        if (/iPhone|iPad|iPod|Android/i.test(navigator.userAgent)) {
+          media = "sm";
+        } else if (w <= 640) {
+          media = "sm";
+        } else if (w <= 768) {
+          media = "md";
+        } else if (w <= 1024) {
+          media = "lg";
+        } else if (w <= 1280) {
+          media = "xl";
+        } else {
+          media = "2xl";
+        }
+
+        setDimension({
+          width: w,
+          height: h,
+          media,
+        });
       }
-
-      setDimension({
-        width: w,
-        height: h,
-        media,
-      });
     };
 
     handleResize(window);
 
     window.addEventListener("resize", handleResize);
 
-    return () => document?.removeEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
   }, []);
 
   return { dimension };
